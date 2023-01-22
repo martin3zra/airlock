5e1ae2720c71d0b2bef3e268e070ef4952bf36bb
Alfredo Martínez <martin3zra@gmail.com>
2023-01-21 22:43:52 -0400

    grab token from cookie or headers

--- request.go
+++ request.go
@@ -12,22 +12,21 @@ import (
 
 func (a *auth) tokenFromRequest(r *http.Request) (string, error) {
 	var token string
-	if a.config.storeInCookie {
+	tokens, ok := r.Header["Authorization"]
+	if ok && len(tokens) >= 1 {
+		token = tokens[0]
+		token = strings.TrimPrefix(token, "Bearer ")
+	}
+
+	if len(token) == 0 {
 		cookie, err := r.Cookie("token")
 		if err != nil {
 			return "", new(AccessTokenMissing)
 		}
 		token = cookie.Value
-	} else {
-		tokens, ok := r.Header["Authorization"]
-		if ok && len(tokens) >= 1 {
-			token = tokens[0]
-			token = strings.TrimPrefix(token, "Bearer ")
+		if len(token) == 0 {
+			return "", new(AccessTokenMissing)
 		}
-	}
-
-	if len(token) == 0 {
-		return "", new(AccessTokenMissing)
 	}
 
 	return token, nil
